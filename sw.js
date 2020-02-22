2b8c55a0500c6d4e013f9c6a52e38476e4d1ca3d
jlopezcur <jlopezcur@users.noreply.github.com>
2020-02-22 15:51:17 +0000

    deploy: 05c4eda989c9f0e3d66761fcdc419fb234333f78

--- sw.js
+++ sw.js
@@ -27,16 +27,16 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-836dee8eba91b9155b67.js"
+    "url": "webpack-runtime-28858ff54aee6bf4804a.js"
   },
   {
-    "url": "styles.c3d0844eeae38a45797e.css"
+    "url": "styles.e55cdb58a5c4b9325a1c.css"
   },
   {
     "url": "styles-f632712bd76950406749.js"
   },
   {
-    "url": "commons-ec27575f3d69ccc6b503.js"
+    "url": "commons-caa2fe82e670f0d9d1f1.js"
   },
   {
     "url": "app-7159a9b1f4a06be55d68.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "393a84a89186f909924d9a16b9e8fd8c"
+    "revision": "ac3911a043cab3d98c1611292ae546d1"
   },
   {
     "url": "google-fonts/s/rye/v7/r05XGLJT86YzEZ7t.woff2",

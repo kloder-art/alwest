54db27dfdf292dc09b22e5b4873168f829881da4
Javier López Úbeda <jlopezcur@gmail.com>
2020-11-05 22:46:50 +0100

    feat: redirect at index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
+import { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-import Layout from '../components/Layout';
-import SEO from '../components/SEO';
-import Films from '../components/films/Films';
-import Container from '../components/Container';
-
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <Container>
-      <Films items={data.allFile.edges.map(x => x.node.childMarkdownRemark)} />
-    </Container>
-  </Layout>
-);
-
-IndexPage.propTypes = {
-  data: PropTypes.object,
+const IndexPage = () => {
+  useEffect(() => {
+    navigate('/film');
+  });
+  return null;
 };
 
 export default IndexPage;
-
-export const query = graphql`
-  query {
-    allFile(
-      filter: { sourceInstanceName: { eq: "film" }, extension: { eq: "md" } }
-      sort: { fields: childMarkdownRemark___frontmatter___year, order: DESC }
-    ) {
-      edges {
-        node {
-          childMarkdownRemark {
-            frontmatter {
-              id
-              title
-              poster {
-                childImageSharp {
-                  fixed(width: 150, height: 230) {
-                    ...GatsbyImageSharpFixed
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;

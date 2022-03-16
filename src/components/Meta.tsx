6099cee062414e154fb37f62e1e41432ad30601a
Javier López Úbeda <jlopezcur@gmail.com>
2022-03-16 03:05:48 +0100

    refactor: Meta component

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -1,38 +1,34 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { FaEye, FaImdb, FaMusic, FaWikipediaW } from 'react-icons/fa';
+
+const ICON_SIZE = 22;
 
 const formatRuntime = (runtime: number) =>
   runtime > 60
     ? `${Math.floor(runtime / 60)}h. ${runtime % 60}m.`
     : `${runtime}m`;
 
+const formatRecaudation = (x: number) => x.toLocaleString();
+
 const Item: React.FC = ({ children }) => <li>{children}</li>;
 
 interface LinkProps {
   label?: string;
-  children?: string;
+  href?: string;
+  icon?: React.ReactNode;
 }
 
-const Link: React.FC<LinkProps> = ({ children = '', label = '' }) => (
-  <a href={children} target={'_blank'} rel={'noopener noreferrer'}>
-    {label}
+const Link: React.FC<LinkProps> = ({ href = '', label = '', icon }) => (
+  <a href={href} target={'_blank'} rel={'noopener noreferrer'} title={label}>
+    {icon ? icon : label}
   </a>
 );
-
-const items = [
-  { key: 'year' },
-  { key: 'runtime', fn: formatRuntime },
-  { key: 'imdb', label: 'IMDB', Cmp: Link },
-  { key: 'spotify', label: 'BSO', Cmp: Link },
-  { key: 'wikipedia', label: 'Wikipedia', Cmp: Link },
-  { key: 'recaudation', fn: (x: number) => x.toLocaleString() },
-  { key: 'born' },
-];
 
 const StyledMeta = styled.ul`
   display: flex;
   list-style: none;
-  margin: 0;
+  margin: 8px 0;
   li {
     margin-right: 0.5rem;
   }
@@ -43,10 +39,11 @@ interface MetaProps {
     year?: number;
     runtime?: number;
     imdb?: string;
-    sporify?: string;
+    spotify?: string;
     wikipedia?: string;
     recaudation?: number;
     born?: string;
+    justwatch?: string;
   };
 }
 
@@ -58,32 +55,53 @@ interface Item {
   value?: number | string;
 }
 
-const hasKey =
-  (frontmatter: { [key: string]: string | number }) =>
-  ({ key }: Item) =>
-    key in frontmatter;
-
-const appendValue =
-  (frontmatter: { [key: string]: string | number }) => (item: Item) => {
-    return {
-      ...item,
-      value: item.fn ? item.fn(frontmatter[item.key]) : frontmatter[item.key],
-    };
-  };
-
 export const Meta: React.FC<MetaProps> = ({ frontmatter }) => (
   <StyledMeta>
-    {items
-      .filter(hasKey(frontmatter))
-      .map(appendValue(frontmatter))
-      .map(({ Cmp, ...item }: Item) => (
-        <Item key={item.key}>
-          {Cmp ? (
-            <Cmp label={item.label}>{String(item.value)}</Cmp>
-          ) : (
-            item.value
-          )}
-        </Item>
-      ))}
+    {frontmatter.year && <Item key={'year'}>{frontmatter.year}</Item>}
+    {frontmatter.runtime && (
+      <Item key={'runtime'}>{formatRuntime(frontmatter.runtime)}</Item>
+    )}
+    {frontmatter.imdb && (
+      <Item key={'imdb'}>
+        <Link
+          label={'IMDB'}
+          icon={<FaImdb size={ICON_SIZE} />}
+          href={frontmatter.imdb}
+        />
+      </Item>
+    )}
+    {frontmatter.justwatch && (
+      <Item key={'justwatch'}>
+        <Link
+          label={'JustWatch'}
+          icon={<FaEye size={ICON_SIZE} />}
+          href={frontmatter.justwatch}
+        />
+      </Item>
+    )}
+    {frontmatter.spotify && (
+      <Item key={'spotify'}>
+        <Link
+          label={'BSO'}
+          icon={<FaMusic size={ICON_SIZE} />}
+          href={frontmatter.spotify}
+        />
+      </Item>
+    )}
+    {frontmatter.wikipedia && (
+      <Item key={'wikipedia'}>
+        <Link
+          label={'Wikipedia'}
+          icon={<FaWikipediaW />}
+          href={frontmatter.wikipedia}
+        />
+      </Item>
+    )}
+    {frontmatter.recaudation && (
+      <Item key={'recaudation'}>
+        {formatRecaudation(frontmatter.recaudation)}
+      </Item>
+    )}
+    {frontmatter.born && <Item key={'born'}>{frontmatter.born}</Item>}
   </StyledMeta>
 );
